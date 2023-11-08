8fd0b2f374a937d0e72186b1916dd1706ab2423d
DanielDavB <danieldavilabarrios@gmail.com>
2023-11-08 00:11:14 -0600

    working scraper

--- scraper.py
+++ scraper.py
@@ -2,26 +2,33 @@ import requests
 import lxml.html as html 
 import datetime
 import os
+import time
+import re
 
-HOME_URL = "https://cyware.com/cyber-security-news-articles"
-XPATH_LINK_TO_ARTICLE = '//div[@class="cy-panel__body"]/a/@href'
-XPATH_TITLE = '//h1[@class="cy-card__title m-0 cursor-pointer pb-3"]/text()'
-XPATH_SUMMARY = '//div[@class="cy-card__description"]/text()'
-XPATH_BODY = '//div[@class="zox-post-body left zoxrel zox100"]/p/text()'
+HOME_URL = "https://thehackernews.com/"
+XPATH_LINK_TO_ARTICLE = '//a[@class="story-link"]/@href'
+XPATH_TITLE = '//h1[@class="story-title"]//text()'
+XPATH_SUMMARY = '//div[@class="articlebody clear cf"]/p[1]/text()'
+XPATH_BODY = '//div[@class="articlebody clear cf"]/p/text()'
 
+def sanitize_title(title):
+    # Remove characters that are not allowed in filenames
+    title = re.sub(r'[\/:*?"<>|]', '', title)
+    # Replace spaces with underscores
+    title = title.replace(' ', '_')
+    return title
 
-def parse_notice(link,today):
+def parse_notice(link, today):
     try:
         response = requests.get(link)
         if response.status_code == 200:
-            print('si jalo')
             notice = response.content.decode('utf-8')
             parsed = html.fromstring(notice)
             
             try:
                 title = parsed.xpath(XPATH_TITLE)[0]
-                title = title.replace('\"', '')
-                title = title.replace('\n', '')
+                title = title.replace('\n', '').strip()
+                title = sanitize_title(title)  # Sanitize the title
                 summary = parsed.xpath(XPATH_SUMMARY)[0]
                 body = parsed.xpath(XPATH_BODY)
             except IndexError:
@@ -29,7 +36,6 @@ def parse_notice(link,today):
             with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
                 f.write(title)
                 f.write('\n\n')
-                f.write(summary)
                 f.write('\n\n')
                 for p in body:
                     f.write(p)
@@ -38,17 +44,14 @@ def parse_notice(link,today):
             raise ValueError(f'Error: {response.status_code}')    
     except ValueError as ve:
         print(ve)
-            
 
-
-def parse_home(): #Get links
+def parse_home():
     try:
         response = requests.get(HOME_URL)
         if response.status_code == 200:
             home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
             links_to_notice = parsed.xpath(XPATH_LINK_TO_ARTICLE)
-            #print(links_to_notice)
             
             today = datetime.date.today().strftime('%d-%m-%Y')
             if not os.path.isdir(today):
@@ -61,11 +64,8 @@ def parse_home(): #Get links
     except ValueError as ve:
         print(ve)
 
-
 def run():
     parse_home()
 
-
 if __name__ == '__main__':
     run()
-    
