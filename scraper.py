b022f63fd3b7e6b5bbd37d65b6aec554bc7a23d9
DanielDavB <danieldavilabarrios@gmail.com>
2023-11-14 13:00:30 -0600

    solo falta que suba datos

--- scraper.py
+++ scraper.py
@@ -4,12 +4,35 @@ import datetime
 import os
 import time
 import re
+import supabase
 
 HOME_URL = "https://thehackernews.com/"
 XPATH_LINK_TO_ARTICLE = '//a[@class="story-link"]/@href'
 XPATH_TITLE = '//h1[@class="story-title"]//text()'
-XPATH_SUMMARY = '//div[@class="articlebody clear cf"]/p[1]/text()'
-XPATH_BODY = '//div[@class="articlebody clear cf"]/p/text()'
+
+# Supabase configuration
+SUPABASE_URL = "https://db.asniuqxpkwgyoyprturw.supabase.co"
+SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzbml1cXhwa3dneW95cHJ0dXJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk5ODQ1MjYsImV4cCI6MjAxNTU2MDUyNn0.2XVpfJvrAdrLt7sZ8_WlSbG8v_s9kbJoy9yQx-IkmWo"
+SUPABASE_PROJECT_NAME = "infosecur"
+SUPABASE_TABLE_NAME = "data_links"
+
+def insert_into_database(title, link):
+    try:
+        # Connect to Supabase
+        supabase_client = supabase.create_client(
+            SUPABASE_URL,
+            SUPABASE_API_KEY,
+            SUPABASE_PROJECT_NAME
+        )
+
+        # Insert into the database
+        response = supabase_client.table(SUPABASE_TABLE_NAME).upsert([{"title": title, "link": link}], returning="minimal")
+
+        if response.status_code != 200:
+            print("Error inserting into database:", response.text)
+
+    except Exception as e:
+        print("Error:", e)
 
 def sanitize_title(title):
     # Remove characters that are not allowed in filenames
@@ -29,17 +52,15 @@ def parse_notice(link, today):
                 title = parsed.xpath(XPATH_TITLE)[0]
                 title = title.replace('\n', '').strip()
                 title = sanitize_title(title)  # Sanitize the title
-                summary = parsed.xpath(XPATH_SUMMARY)[0]
-                body = parsed.xpath(XPATH_BODY)
+                
+                print(link, title)
+                # Insert into the database
+                insert_into_database(title, link)
+
             except IndexError:
                 return
-            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
-                f.write(title)
-                f.write('\n\n')
-                f.write('\n\n')
-                for p in body:
-                    f.write(p)
-                    f.write('\n')
+            
+                
         else:
             raise ValueError(f'Error: {response.status_code}')    
     except ValueError as ve:
